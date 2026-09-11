6f4c655c08719a501ec17ac82bbc04040018fc94
Ivan Litvin <ivanlitvin51@gmail.com>
2026-09-11 23:13:02 +0700

    oop: fix hw_01 (random)

--- OOP/hw_01/hypo_mod.cpp
+++ OOP/hw_01/hypo_mod.cpp
@@ -2,13 +2,19 @@
 #include "hypo.hpp"
 #include <random>
 
-static std::random_device rnd;
-
 double hypotenuse_mod(int a, int b) {
+    // 1. Генератор псевдослучайных чисел (Mersenne Twister),
+    // инициализируемый зерном от железа/ОС один раз при первом вызове
+    static std::mt19937 gen(std::random_device{}());
+
+    // 2. Равномерные распределения (математически честные, без смещения остатка)
+    static std::uniform_int_distribution<int> coin(0, 1);    // 50% шанс модификации (0 или 1)
+    static std::uniform_int_distribution<int> bonus(1, 10);  // прибавка от 1 до 10 включительно
+
     double res = hypotenuse(a, b);
 
-    if (rnd() % 2 == 0) {
-        res += rnd() % 10 + 1;
+    if (coin(gen) == 0) {
+        res += bonus(gen);
     }
 
     return res;
